437b1389ec13cd6f53133cbf24c49300416616ba
Roman Polupanov <87490407+Roman-Polupanov@users.noreply.github.com>
2021-12-03 00:13:43 +0200

    fix mui

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export default function MainPage(props) {
     return (
-        <table className="main-page__table">
-           {props.data?.map((item) => <tr className="main-page__item">
-               <td>Ticker: <strong>{item?.ticker}</strong></td>
-               <td>Percent: <strong>{item?.change_percent}</strong></td>
-               <td>Dividend: <strong>{item?.dividend}</strong></td>
-               <td>Exchange: <strong>{item?.exchange}</strong></td>
-               <td>Last trade: <strong>{new Date(item?.last_trade_time).toLocaleString()}</strong></td>
-               <td>Price: <strong>{item?.price}</strong></td>
-               <td>Yield: <strong>{item?.yield}</strong></td>
-           </tr>)}
-        </table>
-    )
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="right">Ticker</TableCell>
+                        <TableCell align="right">Percent</TableCell>
+                        <TableCell align="right">Dividend</TableCell>
+                        <TableCell align="right">Exchange</TableCell>
+                        <TableCell align="right">Last trade</TableCell>
+                        <TableCell align="right">Price</TableCell>
+                        <TableCell align="right">Yield</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {props.data?.map((item) => (
+                        <TableRow
+                            key={item?.ticker}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell align="right">{item?.ticker}</TableCell>
+                            <TableCell align="right">{item?.change_percent}</TableCell>
+                            <TableCell align="right">{item?.dividend}</TableCell>
+                            <TableCell align="right">{item?.exchange}</TableCell>
+                            <TableCell align="right">{item?.last_trade_time}</TableCell>
+                            <TableCell align="right">{item?.price}</TableCell>
+                            <TableCell align="right">{item?.yield}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
